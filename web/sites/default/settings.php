bb973c319a5b696191ca761db9f1d85643104da3
martinyde <yde001@gmail.com>
2025-04-01 08:31:54 +0200

    Added deactivation of permisisons hardening

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -836,3 +836,4 @@ if (file_exists($app_root . "/" . $site_path . "/settings.local.php")) {
   include $app_root . "/" . $site_path . "/settings.local.php";
 }
 
+$settings['skip_permissions_hardening'] = TRUE;
